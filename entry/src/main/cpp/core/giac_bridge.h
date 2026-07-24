790b8f0c173ab0182e93d491f24c952828af530c
StartYi <yirui.pre@gmail.com>
2026-07-24 13:17:45 +0800

    重构giac_bridge

--- entry/src/main/cpp/core/giac_bridge.h
+++ entry/src/main/cpp/core/giac_bridge.h
@@ -7,8 +7,5 @@
  */
 #pragma once
 #include <string>
-#include "json.hpp"
 
 std::string evaluateWithGiac(const std::string& mathExpression);
-// 暂且保留的构建指令函数
-std::string buildGiacCommand(const nlohmann::json& ast);
